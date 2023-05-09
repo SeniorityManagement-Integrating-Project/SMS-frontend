bb211312c22efc66c69e9419dd08fde744a56430
Mateo Rivera <mateo.rivera@udea.edu.co>
2023-05-08 22:00:50 -0500

    Getting error when user does not have the scope

--- src/pages/api/skills.js
+++ src/pages/api/skills.js
@@ -4,16 +4,21 @@ import { getAccessToken, withApiAuthRequired } from '@auth0/nextjs-auth0';
 export default withApiAuthRequired(async function skills(req, res) {
   // If your access token is expired and you have a refresh token
   // `getAccessToken` will fetch you a new one using the `refresh_token` grant
-  const { accessToken } = await getAccessToken(req, res, {
-    scopes: ['read:skills']
-  });
-  console.log(accessToken);
+  try {
+    const { accessToken } = await getAccessToken(req, res, {
+      scopes: ['read:skills']
+    });
     //const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/skill/`, {
-    const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/protected`, {
-    headers: {
-      Authorization: `Bearer ${accessToken}`
-    }
-  });
-  const skills = await response.json();
-  res.status(200).json(skills);
+      const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/protected`, {
+        headers: {
+          Authorization: `Bearer ${accessToken}`
+        }
+      });
+      const skills = await response.json();
+      res.status(200).json(skills);
+  } catch (err) {
+    console.error(err);
+    res.status(401).json({ message: "No estás autorizado para acceder a este recurso" });
+  }
+    
 });
