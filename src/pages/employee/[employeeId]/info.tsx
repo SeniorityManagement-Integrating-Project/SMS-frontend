6b6c871ff7876cf6261e12ff71d8e09f75f0c131
Mateo Rivera <mateo.rivera@udea.edu.co>
2023-05-19 14:31:46 -0500

    Adding styles

--- src/pages/employee/[employeeId]/info.tsx
+++ src/pages/employee/[employeeId]/info.tsx
@@ -1,3 +1,4 @@
+import LoginLogout from '@/components/LoginLogout';
 import { useUser } from '@auth0/nextjs-auth0/client';
 import { create } from 'domain';
 import { useRouter } from 'next/router';
@@ -68,40 +69,73 @@ const info = () => {
   }, []);
 
   return (
-    <form onSubmit={handleSubmit}>
-      <div>
-        <label htmlFor='employee-name'>
-          <span>Full name</span>
-          <input id='employee-name' value={form.name} disabled />
-        </label>
+    <div className='bg-slate-900 justify-center items-center h-screen w-screen flex flex-col'>
+      <div className='bg-slate-400 rounded-2xl w-96 p-10 flex flex-col'>
+        <span className='text-2xl'>Welcome to our Seniority Management System</span>
+        <span className='mb-6'>Complete your information for knowing your growth path</span>
+        <form onSubmit={handleSubmit}>
+          <div>
+            <label htmlFor='employee-name' className='flex flex-col mb-6'>
+              <span>Full name</span>
+              <input
+                id='employee-name'
+                value={form.name}
+                disabled
+                className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg w-full p-2.5 focus:border-b-rose-600 focus:outline-rose-600'
+              />
+            </label>
+          </div>
+          <div>
+            <label htmlFor='employee-email' className='flex flex-col mb-6'>
+              <span>Email</span>
+              <input
+                id='employee-email'
+                value={form.email}
+                disabled
+                className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg w-full p-2.5 focus:border-b-rose-600 focus:outline-rose-600'
+              />
+            </label>
+          </div>
+          <div>
+            <label htmlFor='employee-role' className='flex flex-col mb-6'>
+              <span>Role in the Company</span>
+              <select
+                id='employee-role'
+                value={form.role}
+                onChange={handleRoleChange}
+                required
+                className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg w-full p-2.5 focus:border-b-rose-600 focus:outline-rose-600'
+              >
+                <option value=''>Select a role</option>
+                {roles.map((role) => (
+                  <option key={role.id} value={role.id}>
+                    {role.name}
+                  </option>
+                ))}
+              </select>
+            </label>
+          </div>
+          <div>
+            <label htmlFor='employee-biography'>
+              <span>Biography</span>
+              <textarea
+                id='employee-biography'
+                value={form.biography}
+                onChange={handleBiographyChange}
+                required
+                className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg w-full p-2.5 focus:border-b-rose-600 focus:outline-rose-600'
+              />
+            </label>
+          </div>
+          <div className='flex justify-between'>
+            <button type='submit' className='w-32 px-5 py-2 text-sm text-white rounded-md bg-rose-600'>
+              Submit
+            </button>
+            <LoginLogout />
+          </div>
+        </form>
       </div>
-      <div>
-        <label htmlFor='employee-email'>
-          <span>Email</span>
-          <input id='employee-email' value={form.email} disabled />
-        </label>
-      </div>
-      <div>
-        <label htmlFor='employee-role'>
-          Role in the Company
-          <select id='employee-role' value={form.role} onChange={handleRoleChange} required>
-            <option value=''>Select a role</option>
-            {roles.map((role) => (
-              <option key={role.id} value={role.id}>
-                {role.name}
-              </option>
-            ))}
-          </select>
-        </label>
-      </div>
-      <div>
-        <label htmlFor='employee-biography'>
-          <span>Biography</span>
-          <textarea id='employee-biography' value={form.biography} onChange={handleBiographyChange} required />
-        </label>
-      </div>
-      <button type='submit'>Submit</button>
-    </form>
+    </div>
   );
 };
 
